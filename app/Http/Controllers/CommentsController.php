b9764bd7fa4d5ae3f038753d757d40b5872f5a25
hackemate <nitrowitty@gmail.com>
2017-03-03 12:22:36 -0600

    ultimo push

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -8,8 +8,15 @@ use App\Comment;
 class CommentsController extends Controller
 {
     public function store(Post $post){
-        $this->validate(request(),['body' => 'required|min:2']);
-        $post->addComment(request('body'));
+        $this->validate(request(),[
+            'body' => 'required|min:2'
+        ]);
+
+        Comment::create([
+            'body' => request('body'),
+            'user_id' => auth()->id(),
+            'post_id' => $post->id()
+        ]);
         return back();
     }
 }
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,8 +2,10 @@
      <div class="container">
        <nav class="nav blog-nav">
          <a class="nav-link active" href="/">Home</a>
-         <a class="nav-link" href="/register">register</a>
-         <a class="nav-link" href="/login">login</a>
+         @if (!Auth::check())
+             <a class="nav-link" href="/register">register</a>
+             <a class="nav-link" href="/login">login</a>
+         @endif
          @if (Auth::check())
              <a class="nav-link" href="/logout">logout</a>
              <a class="nav-link" href="/posts/create">create</a>
